8fa57176e8754fe1b66b1b242b74e4e2565d3b31
Balaji Damodaran <damodaran.balaji@gmail.com>
2011-10-20 23:19:30 +0530

    added a spanking dark theme for intellij 10.5 ce and powershell profile

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,24 @@
+#git aliases
+set-alias g "git"
+function gst { git status $args }
+function gpl { git pull $args }
+function gpu { git push $args }
+function ga { git add $args }
+function gr { git rm $args }
+function gme { git merge $args }
+function gcp { git cherry-pick $args }
+function gsh { git stash $args }
+function gco { git commit $args }
+
+#motech commands
+remove-item alias:h
+function w {cd C:\Users\balajid\Documents\thoughtworks\motech\GHANA-National }
+
+#mvn aliases
+function m { mvn $args }
+function mci { mvn clean install $args }
+function mjr { mvn jetty:run }
+
+#common
+set-alias c "cls"
+function h { cd ~ }
