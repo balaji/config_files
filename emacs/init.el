3f2f413329ca87485c8bba1f435c8c0d02ddfa62
Balaji Damodaran <damodaran.balaji@gmail.com>
2013-02-17 20:56:45 +0530

    slime for common lisp. nrepl for clojure

--- emacs/init.el
+++ emacs/init.el
@@ -2,36 +2,68 @@
 ;; starter-kit
 ;; starter-kit-lisp
 ;; solarized-theme
-;; fsharp-mode
+;; rainbow-mode
+;; nrepl
+;; auto-complete
+;; ac-nrepl
+;; clojure-mode
 (require 'package)
 (setq package-archives
-      '(("melpa" . "http://melpa.milkbox.net/packages/")
-        ("marmalade" . "http://marmalade-repo.org/packages/")
-        ("gnu" . "http://elpa.gnu.org/packages/")))
+      '(("marmalade" . "http://marmalade-repo.org/packages/")))
 (package-initialize)
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-;;slime and clisp
-(setq inferior-lisp-program "~/clisp-2.49/clisp.exe")
-(add-to-list 'load-path "~/slime")
+;;common-lisp
+(setq inferior-lisp-program "/usr/local/bin/sbcl")
+(add-to-list 'load-path "~/Programs/slime")
 (require 'slime)
 (slime-setup)
 (slime-setup '(slime-fancy))
 
-;;solarized 
+;;solarized
 (require 'solarized-dark-theme)
+
+;;paredit
+(add-hook 'clojure-mode-hook 'paredit-mode)
+(add-hook 'nrepl-mode-hook 'paredit-mode)
+(add-hook 'slime-repl-mode-hook 'paredit-mode)
+(global-set-key [f7] 'paredit-mode)
+
+;; nrepl
+(add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
+(setq nrepl-popup-stacktraces nil)
+(add-to-list 'same-window-buffer-names "*nrepl*")
+(add-hook 'nrepl-mode-hook 'paredit-mode)
+(global-set-key [f9] 'nrepl-jack-in)
+
+;; auto-complete
+(require 'auto-complete-config)
+(ac-config-default)
+(define-key ac-completing-map "\M-/" 'ac-stop)
+
+;;ac-nrepl
+(require 'ac-nrepl)
+(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
+(add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
+(eval-after-load "auto-complete" '(add-to-list 'ac-modes 'nrepl-mode))
 
 ;; line numbers enabled
 (global-linum-mode 1)
+(global-rainbow-delimiters-mode)
 
-(setq inferior-fsharp-program "\"C:\\Program Files (x86)\\Microsoft SDKs\\F#\\3.0\\Framework\\v4.0\\Fsi.exe\"")
-(setq fsharp-compiler "\"C:\\Program Files (x86)\\Microsoft SDKs\\F#\\3.0\\Framework\\v4.0\\Fsc.exe\"")
-
-;; font to consolas
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(menu-bar-mode t)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 98 :width normal)))))
+ '(default ((t (:family "Source Code Pro" :foundry "outline" :slant normal :weight normal :height 125 :width normal)))))
