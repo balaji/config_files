eb2cac334b2638d0185f3b2e196c5ef414453bfb
Balaji Damodaran <balaji.damodaran@outlook.com>
2015-01-23 19:55:53 +0530

    modified vim and emacs

--- emacs/init.el
+++ emacs/init.el
@@ -1,56 +1,35 @@
-;; package manager. installed packages:-
-;; starter-kit
-;; starter-kit-lisp
-;; solarized-theme
-;; rainbow-mode
-;; nrepl
-;; auto-complete
-;; ac-nrepl
-;; clojure-mode
+(add-to-list 'load-path "/Users/balaji/Projects/slime")
+(add-to-list 'load-path "~/.emacs.d/vendor/powerline")
+(require 'slime)
+(setq inferior-lisp-program "/usr/local/bin/ccl")
+(slime-setup)
+(slime-setup '(slime-fancy))
+
+(require 'powerline)
+(powerline-center-evil-theme)
 (require 'package)
-(setq package-archives
-      '(("marmalade" . "http://marmalade-repo.org/packages/")))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-;;common-lisp
-(setq inferior-lisp-program "/usr/local/bin/sbcl")
-(add-to-list 'load-path "~/Programs/slime")
-(require 'slime)
-(slime-setup)
-(slime-setup '(slime-fancy))
+(require 'color-theme)
+(color-theme-monokai)
+(evil-mode)
+(global-company-mode)
+(global-linum-mode)
+(setq linum-format "%d ")
 
-;;solarized
-(require 'solarized-dark-theme)
-
-;;paredit
 (add-hook 'clojure-mode-hook 'paredit-mode)
-(add-hook 'nrepl-mode-hook 'paredit-mode)
 (add-hook 'slime-repl-mode-hook 'paredit-mode)
+(add-hook 'cider-mode-hook 'paredit-mode)
 (global-set-key [f7] 'paredit-mode)
+(global-set-key [f6] 'evil-mode)
 
-;; nrepl
-(add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
-(setq nrepl-popup-stacktraces nil)
-(add-to-list 'same-window-buffer-names "*nrepl*")
-(add-hook 'nrepl-mode-hook 'paredit-mode)
-(global-set-key [f9] 'nrepl-jack-in)
-
-;; auto-complete
-(require 'auto-complete-config)
-(ac-config-default)
-(define-key ac-completing-map "\M-/" 'ac-stop)
-
-;;ac-nrepl
-(require 'ac-nrepl)
-(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
-(add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
-(eval-after-load "auto-complete" '(add-to-list 'ac-modes 'nrepl-mode))
-
-;; line numbers enabled
-(global-linum-mode 1)
-(global-rainbow-delimiters-mode)
+(add-hook 'cider-interaction-mode-hook 'cider-turn-on-eldoc-mode)
+(setq cider-popup-stacktraces nil)
+(add-to-list 'same-window-buffer-names "*cider*")
+(global-set-key [f9] 'cider-jack-in)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -58,12 +37,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(blink-cursor-mode nil)
- '(menu-bar-mode t)
  '(show-paren-mode t)
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
+ ;; Your init file should contain one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Source Code Pro" :foundry "outline" :slant normal :weight normal :height 125 :width normal)))))
+ )
+(set-default-font "Source Code Pro-14")
--- vim/.vimrc
+++ vim/.vimrc
@@ -1,6 +1,5 @@
 set nocompatible              
 filetype off                 
-filetype plugin indent on
 syntax on
 set number 
 set incsearch
@@ -24,15 +23,38 @@ set noerrorbells
 set completeopt=longest,menuone
 set omnifunc=syntaxcomplete#Complete
 
-set runtimepath+=~/.vim/bundle/vundle
+"neocomplcache
+" Disable AutoComplPop.
+let g:acp_enableAtStartup = 0
+" Use neocomplcache.
+let g:neocomplcache_enable_at_startup = 1
+" Use smartcase.
+let g:neocomplcache_enable_smart_case = 1
+" Set minimum syntax keyword length.
+let g:neocomplcache_min_syntax_length = 3
+inoremap <expr><TAB>  pumvisible() ? "\<C-n>" : "\<TAB>"
 
-call vundle#rc()
-Plugin 'gmarik/vundle'
+"pathogen
+execute pathogen#infect()
+
+set runtimepath+=~/.vim/bundle/Vundle.vim
+
+call vundle#begin()
+Plugin 'gmarik/Vundle.vim'
 Plugin 'tpope/vim-rails'
 Plugin 'tpope/vim-fugitive'
+Plugin 'tpope/vim-fireplace'
 Plugin 'tpope/vim-sensible'
 Plugin 'tpope/vim-git'
-Plugin 'altercation/vim-colors-solarized'
+Plugin 'tpope/vim-repeat'
+Plugin 'tpope/vim-projectionist'
+Plugin 'tpope/vim-leiningen'
+Plugin 'tpope/vim-dispatch'
+Plugin 'tpope/vim-bundler'
+Plugin 'tpope/vim-surround'
+Plugin 'tpope/vim-sexp-mappings-for-regular-people'
+Plugin 'guns/vim-sexp'
+Plugin 'guns/vim-clojure-static'
 Plugin 'scrooloose/nerdtree'
 Plugin 'ctrlp.vim'
 Plugin 'bling/vim-airline'
@@ -40,20 +62,14 @@ Plugin 'edkolev/tmuxline.vim'
 Plugin 'scrooloose/syntastic'
 Plugin 'bling/vim-bufferline'
 Plugin 'vim-ruby/vim-ruby'
-Plugin 'MarcWeber/vim-addon-mw-utils'
-Plugin 'tomtom/tlib_vim'
-Plugin 'garbas/vim-snipmate'
-Plugin 'honza/vim-snippets'
-Plugin 'L9'
-Plugin 'FuzzyFinder'
-Plugin 'Valloric/YouCompleteMe'
-Plugin 'Lokaltog/vim-easymotion'
-Plugin 'Raimondi/delimitMate'
+Plugin 'Shougo/neosnippet.vim'
+Plugin 'Shougo/neosnippet-snippets'
+Plugin 'Shougo/neocomplcache.vim'
+Plugin 'kien/rainbow_parentheses.vim'
+Plugin 'tpope/vim-rbenv'
+call vundle#end()
+filetype plugin indent on
 
 if has('gui_running')
   colorscheme monokai
-	set guifont=Source_Code_Pro:h12
 endif
-
-imap ss <esc>a<Plug>snipMateNextOrTrigger
-smap ss <Plug>snipMateNextOrTrigger
