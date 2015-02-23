725fb64d2d7d3d74ecbcb4d862a7f11b0cd15daf
Balaji Damodaran <balaji.damodaran@outlook.com>
2015-02-24 00:46:05 +0530

    emacs with helm & projectile

--- emacs/init.el
+++ emacs/init.el
@@ -1,87 +1,73 @@
 (add-to-list 'exec-path "/usr/local/bin")
-(add-to-list 'load-path "/Users/balaji/Projects/slime")
 (add-to-list 'load-path "~/.emacs.d/vendor/powerline")
 
-;;SLIME for Common Lisp
+;;SLIME
+(add-to-list 'load-path "~/Projects/slime")
 (require 'slime)
 (setq inferior-lisp-program "/usr/local/bin/ccl")
 (slime-setup)
 (slime-setup '(slime-fancy))
 
-;;POWERLINE
-(require 'powerline)
-(powerline-center-evil-theme)
-
-;;PACKAGES
-(require 'package)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-
-(defvar my-packages '(evil
+;;PACKAGE MANAGER
+(defvar my-packages '(better-defaults
                       starter-kit
                       starter-kit-lisp
-                      starter-kit-ruby
-                      cider
-                      evil-paredit
-                      evil-surround
-                      rainbow-delimiters
                       projectile
+                      ag
                       helm
                       helm-projectile
-                      highlight))
+                      helm-ag
+                      company
+                      evil
+                      rainbow-delimiters
+                      clojure-mode))
+(require 'package)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa". "http://melpa.org/packages/"))
 (package-initialize)
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-;;HELM
+;;Helm. Dear Helm.
 (require 'helm)
 (require 'helm-config)
 (helm-mode 1)
 (helm-autoresize-mode t)
 (setq helm-buffers-fuzzy-matching t
+      helm-apropos-fuzzy-match t
+      helm-M-x-fuzzy-match t
       helm-recentf-fuzzy-match t)
+(projectile-global-mode)
+(setq projectile-completion-system 'helm)
+(helm-projectile-on)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "C-c h") 'helm-command-prefix)
 (global-set-key (kbd "C-x b") 'helm-mini)
 (global-set-key (kbd "C-x g") 'helm-projectile-grep)
 (global-set-key (kbd "C-x f") 'helm-projectile-find-file)
-(projectile-global-mode)
-(setq projectile-completion-system 'helm)
-(helm-projectile-on)
-
-(require 'color-theme)
-(color-theme-monokai)
-
-;;EVIL
-(evil-mode)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x o") 'helm-occur)
 
 (global-company-mode)
-(global-linum-mode)
-(setq linum-format "%d ")
+
+;;vim, dear vim.
+(evil-mode)
 
 (add-hook 'slime-repl-mode-hook 'paredit-mode)
 (add-hook 'cider-repl-mode-hook 'paredit-mode)
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+
+;;Global key maps
 (global-set-key [f7] 'paredit-mode)
 (global-set-key [f6] 'evil-mode)
-
-(add-hook 'cider-interaction-mode-hook 'cider-turn-on-eldoc-mode)
-(setq cider-popup-stacktraces nil)
-(add-to-list 'same-window-buffer-names "*cider*")
 (global-set-key [f9] 'cider-jack-in)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(blink-cursor-mode nil)
- '(show-paren-mode t)
- '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain one such instance.
- ;; If there is more than one, they won't work right.
- )
+;;Editor customizations
 (set-default-font "Fira Mono-13")
+(require 'color-theme)
+(color-theme-monokai)
+(global-linum-mode)
+(setq linum-format "%d ")
+(require 'powerline)
+(powerline-center-evil-theme)
+(menu-bar-mode)
