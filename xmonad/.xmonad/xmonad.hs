cda813d21c87d5561a9c429e6a5eedd8e1bd11e9
Balaji Damodaran <damodaran.balaji@gmail.com>
2019-05-30 21:45:38 +0200

    xmonad with tray

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -6,6 +6,7 @@ import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 import XMonad.Hooks.SetWMName
 
+
 main = do
     xmproc <- spawnPipe "xmobar"
 
@@ -13,12 +14,20 @@ main = do
         { manageHook = manageDocks <+> manageHook defaults
         , layoutHook = avoidStruts  $  layoutHook defaults
         , logHook = dynamicLogWithPP xmobarPP
-                        { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "green" "" . shorten 50
-                        }
-        , handleEventHook = handleEventHook defaultConfig <+> docksEventHook
+                    { ppOutput = hPutStrLn xmproc
+                    , ppTitle = xmobarColor "green" "" . shorten 50
+                    }
+        , handleEventHook = handleEventHook defaults <+> docksEventHook
         , startupHook = setWMName "LG3D"
-        }
+        } `additionalKeys`
+        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock;xset dpms force off")
+        , ((mod4Mask .|. shiftMask, xK_t), sendMessage ToggleStruts)
+        , ((mod4Mask .|. shiftMask, xK_o), spawn "amixer -D pulse sset Master 5%+")
+        , ((mod4Mask .|. shiftMask, xK_p), spawn "amixer -D pulse sset Master 5%-")
+        , ((mod4Mask .|. shiftMask .|. controlMask, xK_o), spawn "xbacklight -inc 10")
+        , ((mod4Mask .|. shiftMask .|. controlMask, xK_p), spawn "xbacklight -dec 10")
+        , ((0, xK_Print), spawn "ksnapshot")
+        ]
 
 defaults = defaultConfig
   { terminal = "urxvt"
