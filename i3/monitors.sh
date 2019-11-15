bfb62342a52bcb654dd027941edecb0385b7e0af
Balaji Damodaran <balaji@spotify.com>
2019-11-15 16:44:00 +0100

    better script to switch external monitor

--- i3/monitors.sh
+++ i3/monitors.sh
@@ -1,9 +1,33 @@
 #!/bin/sh
 
-OUTPUT=`xrandr |grep -w 'connected' | grep -v 'eDP1' | awk '/connected/{print $1}'`
+if [ -z "$1" ]
+  then
+      echo "No argument supplied. 'usage: monitors on (or) off'"
+      exit 0
+fi
 
-#xrandr --output ${OUTPUT} --scale 1.5x1.5
-xrandr --output ${OUTPUT} --auto --above eDP1
+external_monitor () {
+    OUTPUT=`xrandr |grep -w 'connected' | grep -v 'eDP1' | awk '/connected/{print $1}'`
 
-#xrandr --output ${OUTPUT} --rotate left
+    if [ -z $OUTPUT ]
+    then
+        echo "No external monitor found"
+        exit 0
+    fi
+}
+
+if [ $1 = 'on' ]
+then
+    external_monitor
+    #xrandr --output ${OUTPUT} --scale 1.5x1.5
+    xrandr --output ${OUTPUT} --auto --above eDP1
+    #xrandr --output ${OUTPUT} --rotate left
+    exit 0
+elif [ $1 = 'off' ]
+then
+     external_monitor
+     xrandr --output $OUTPUT --off
+else
+    echo "usage: monitors on (or) off"
+fi
 
--- i3/single-monitor.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-OUTPUT=`xrandr |grep -w 'connected' | grep -v 'eDP1' | awk '/connected/{print $1}'`
-xrandr --output $OUTPUT --off
