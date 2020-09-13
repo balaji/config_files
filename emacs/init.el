aac4f53634f7af55858c29f60d61c62a0d55ea27
Balaji Damodaran <balaji@spotify.com>
2020-09-13 18:19:25 +0200

    font

--- emacs/init.el
+++ emacs/init.el
@@ -109,7 +109,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Hack" :foundry "SRC" :slant normal :weight normal :height 110 :width normal)))))
+ '(default ((t (:family "Cascadia Code" :foundry "SRC" :slant normal :weight normal :height 100 :width normal)))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
