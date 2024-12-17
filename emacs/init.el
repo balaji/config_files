48c57c696fc0057b66f5d9e77c90505401497fcb
Balaji Damodaran <damodaran.balaji@gmail.com>
2024-12-17 23:04:36 +0530

    one file. blog version

--- emacs/init.el
+++ emacs/init.el
@@ -1,67 +1,85 @@
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name
-        "straight/repos/straight.el/bootstrap.el"
-        (or (bound-and-true-p straight-base-dir)
-            user-emacs-directory)))
-      (bootstrap-version 7))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-(setq straight-use-package-by-default t)
-(straight-use-package 'use-package)
+(defvar elpaca-installer-version 0.8)
+(defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
+(defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
+(defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
+(defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
+                              :ref nil :depth 1
+                              :files (:defaults "elpaca-test.el" (:exclude "extensions"))
+                              :build (:not elpaca--activate-package)))
+(let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
+       (build (expand-file-name "elpaca/" elpaca-builds-directory))
+       (order (cdr elpaca-order))
+       (default-directory repo))
+  (add-to-list 'load-path (if (file-exists-p build) build repo))
+  (unless (file-exists-p repo)
+    (make-directory repo t)
+    (when (< emacs-major-version 28) (require 'subr-x))
+    (condition-case-unless-debug err
+        (if-let* ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
+                  ((zerop (apply #'call-process `("git" nil ,buffer t "clone"
+                                                  ,@(when-let* ((depth (plist-get order :depth)))
+                                                      (list (format "--depth=%d" depth) "--no-single-branch"))
+                                                  ,(plist-get order :repo) ,repo))))
+                  ((zerop (call-process "git" nil buffer t "checkout"
+                                        (or (plist-get order :ref) "--"))))
+                  (emacs (concat invocation-directory invocation-name))
+                  ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
+                                        "--eval" "(byte-recompile-directory \".\" 0 'force)")))
+                  ((require 'elpaca))
+                  ((elpaca-generate-autoloads "elpaca" repo)))
+            (progn (message "%s" (buffer-string)) (kill-buffer buffer))
+          (error "%s" (with-current-buffer buffer (buffer-string))))
+      ((error) (warn "%s" err) (delete-directory repo 'recursive))))
+  (unless (require 'elpaca-autoloads nil t)
+    (require 'elpaca)
+    (elpaca-generate-autoloads "elpaca" repo)
+    (load "./elpaca-autoloads")))
+(add-hook 'after-init-hook #'elpaca-process-queues)
+(elpaca `(,@elpaca-order))
+(elpaca elpaca-use-package
+  ;; Enable use-package :ensure support for Elpaca.
+  (elpaca-use-package-mode))
 
-(use-package better-defaults)
+(if (eq system-type 'darwin)
+    ;;; mac osx
+    (progn
+      (setq projects-path '("~/projects" "~/stripe"))
+      )
+  (if (eq system-type 'gnu/linux)
+      ;;; linux
+      (progn
+	(setq projects-path '("~/projects"))
+	)
+    ;;; windows
+    (progn
+      (setq lsp-erlang-ls-server-path "c:/users/balaj/bin/erlang_ls.cmd")
+      (setq projects-path '("~/projects"))
+      )))
+(use-package better-defaults :ensure t)
 
 (use-package emacs
   :custom
   (enable-recursive-minibuffers t)
   (read-extended-command-predicate #'command-completion-default-include-p)
+  (tab-always-indent 'complete)
+  (text-mode-ispell-word-completion nil)
 
   :init
   (setq inhibit-startup-screen 1)
   (setq vc-follow-symlinks t)
   (cua-mode t)
   (recentf-mode t)
-
+  (global-display-line-numbers-mode t)
+  (add-to-list 'default-frame-alist '(fullscreen . maximized))
   (setq minibuffer-prompt-properties
         '(read-only t cursor-intangible t face minibuffer-prompt))
   (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
 
-  (if (version< emacs-version "29")
-      (global-linum-mode 1)
-    (global-display-line-numbers-mode t))
-
-  (if (eq system-type 'darwin)
-    ;;; mac osx
-      (progn
-        (setq erlang-installed-path "/opt/homebrew/lib/erlang")
-        (setq rebar3-installed-path "~/.cache/rebar3/bin/rebar3")
-        (setq projects-path '("~/projects" "~/stripe"))
-        )
-    (if (eq system-type 'gnu/linux)
-      ;;; linux
-        (progn
-	  (setq erlang-installed-path "/usr/local/lib/erlang")
-	  (setq rebar3-installed-path "~/.cache/rebar3/bin/rebar3")
-	  (setq projects-path '("~/projects"))
-	  )
-    ;;; windows
-      (progn
-        (setq erlang-installed-path "c:/users/balaj/scoop/apps/erlang/current")
-        (setq rebar3-installed-path "c:/users/balaj/bin/rebar3.cmd")
-        (setq lsp-erlang-ls-server-path "c:/users/balaj/bin/erlang_ls.cmd")
-        (setq projects-path '("~/projects"))
-        )))
   (defun my/on-window-display ()
     (if (display-graphic-p)
-        (progn (set-frame-font "Cascadia Mono 11" nil t)
-               (menu-bar-mode 1))
+        (progn
+          (set-frame-font "Cascadia Mono 12" nil t)
+          (menu-bar-mode 1))
       (progn
         (menu-bar-mode 0))))
 
@@ -69,18 +87,6 @@
             (lambda (frame)
               (select-frame frame)
               (my/on-window-display)))
-  (add-hook 'after-init-hook #'my/on-window-display)
-  )
-
-
-(straight-use-package
- '(eat :type git
-       :host codeberg
-       :repo "akib/emacs-eat"
-       :files ("*.el" ("term" "term/*.el") "*.texi"
-               "*.ti" ("terminfo/e" "terminfo/e/*")
-               ("terminfo/65" "terminfo/65/*")
-               ("integration" "integration/*")
-               (:exclude ".dir-locals.el" "*-tests.el"))))
+  (add-hook 'after-init-hook #'my/on-window-display))
 
 (mapc 'load (file-expand-wildcards "~/projects/config_files/emacs/load/*.el"))
--- emacs/load/init-editor.el
+++ emacs/load/init-editor.el
@@ -1,25 +1,78 @@
 (use-package ace-window
+  :ensure t
   :config
   (global-set-key (kbd "M-o") 'ace-window)
   (setq aw-dispatch-always t)
   (ace-window-display-mode))
 
+(use-package avy
+  :ensure t)
+
+(use-package cape
+  :ensure t
+  :bind ("C-c p" . cape-prefix-map))
+
 (use-package consult
+  :ensure t
   :bind
   (("C-x b" . consult-buffer)
    ("C-s" . consult-line)
    ("C-c r" . consult-recent-file)))
 
-(use-package avy)
+(use-package corfu
+  :ensure t
+  :bind
+  (:map corfu-map ("SPC" . corfu-insert-separator))
+  :init
+  (global-corfu-mode))
 
 (use-package doom-themes
+  :ensure t
   :config (load-theme 'doom-vibrant t))
 
 (use-package doom-modeline
+  :ensure t
   :config
   (doom-modeline-mode))
 
+(elpaca
+    (eat :type git
+         :host codeberg
+         :repo "akib/emacs-eat"
+         :files ("*.el" ("term" "term/*.el") "*.texi"
+                 "*.ti" ("terminfo/e" "terminfo/e/*")
+                 ("terminfo/65" "terminfo/65/*")
+                 ("integration" "integration/*")
+                 (:exclude ".dir-locals.el" "*-tests.el"))))
+
+(use-package elpy
+  :ensure t)
+
+(use-package embark
+  :ensure t
+  :bind
+  (("C-'" . embark-act)
+   ("C-;" . embark-dwim)
+   ("C-h b" . embark-bindings))
+  :init
+  (setq prefix-help-command #'embark-prefix-help-command)
+  :config
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+(use-package embark-consult
+  :ensure t
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
+
+(use-package erlang
+  :ensure t)
+
 (use-package evil
+  :ensure t
   :init
   (setq evil-want-integration t)
   (setq evil-want-keybinding nil)
@@ -29,58 +82,165 @@
   (evil-mode t))
 
 (use-package evil-collection
+  :ensure t
   :after evil
   :config
   (evil-collection-init))
 
 (use-package expand-region
+  :ensure t
   :config
   (global-set-key (kbd "C-=") 'er/expand-region)
   (global-set-key (kbd "C--") 'er/contract-region))
 
 (use-package find-file-in-project
+  :ensure t
   :config
+  (setq ffip-match-path-instead-of-filename t)
   (setq ffip-use-rust-fd t))
+
+(use-package flycheck
+  :ensure t
+  :init
+  (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
+  (setq flycheck-ruby-rubocop-executable "/Users/bala/stripe/pay-server/scripts/bin/rubocop.rb")
+  (setq flycheck-ruby-executable "/Users/bala/.rbenv/shims/ruby")
+  :config
+  (global-flycheck-mode 1))
 
 (use-package general
   :ensure t
   :config
   (general-auto-unbind-keys)
-
-  (defconst slk/emacs-leader-global-prefix "C-c")
   (general-define-key
    :states '(emacs insert normal)
    :prefix-map 'custom/prefix
    :global-prefix "C-c"
    :non-normal-prefix "M-SPC"
-   :prefix "SPC")
+   :prefix "SPC"
+   "ff" 'find-file-in-project-by-selected
+   "sf" 'consult-fd
+   "sr" 'consult-ripgrep
+   "so" 'consult-outline
+   "sc" 'avy-goto-char
+   "sw" 'avy-goto-word-1
+   "sl" 'avy-goto-line
+   "pp" 'whaler
+   "wr" 'whaler-populate-projects-directories)
 
-  (general-create-definer leader-global
-    :keymaps 'custom/prefix))
+  (general-create-definer leader-global :keymaps 'custom/prefix))
 
-(use-package magit)
+(use-package lsp-mode
+  :ensure t
+  :custom
+  (lsp-completion-provider :none) ;; we use Corfu!
+  :init
+  (setq lsp-keymap-prefix "C-l")
+  (setq lsp-log-io t)
+  (defun my/orderless-dispatch-flex-first (_pattern index _total)
+    (and (eq index 0) 'orderless-flex))
+
+  (defun my/lsp-mode-setup-completion ()
+    (setf (alist-get 'styles (alist-get 'lsp-capf completion-category-defaults))
+          '(orderless))
+    ;; Optionally configure the first word as flex filtered.
+    (add-hook 'orderless-style-dispatchers #'my/orderless-dispatch-flex-first nil 'local)
+    ;; Optionally configure the cape-capf-buster.
+    (setq-local completion-at-point-functions (list (cape-capf-buster #'lsp-completion-at-point))))
+  :config
+  ;; Decides if the buffer is Ruby and in pay server
+  (defun activate-pay-server-sorbet-p (filename mode)
+    (and
+     (string-prefix-p (expand-file-name "~/stripe/pay-server")
+                      filename)
+     (or (eq major-mode 'ruby-mode) (eq major-mode 'enh-ruby-mode))))
+
+  ;; Configure the connection to Sorbet
+  (lsp-register-client
+   (make-lsp-client :new-connection (lsp-stdio-connection '("pay" "exec" "scripts/bin/typecheck" "--lsp" "--enable-all-experimental-lsp-features"))
+                    :major-modes '(ruby-mode enh-ruby-mode)
+                    :priority 25
+                    :activation-fn 'activate-pay-server-sorbet-p
+                    :server-id 'stripe-sorbet-lsp))
+
+  :hook
+  ((erlang-mode ruby-mode enh-ruby-mode) . 'lsp)
+  (lsp-completion-mode . my/lsp-mode-setup-completion))
+
+(use-package lsp-pyright
+  :ensure t
+  :custom (lsp-pyright-langserver-command "pyright") ;; or basedpyright
+  :hook (python-mode . (lambda ()
+                         (require 'lsp-pyright)
+                         (lsp))))
+
+(use-package lsp-ui
+  :ensure t
+  :config
+  (setq lsp-ui-sideline-enable t)
+  (setq lsp-ui-doc-enable t)
+  (setq lsp-ui-doc-position 'bottom))
+
+(with-eval-after-load 'lsp-mode
+  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration))
+
+(use-package magit
+  :ensure t)
 
 (use-package marginalia
+  :ensure t
   :bind (:map minibuffer-local-map
               ("M-A" . marginalia-cycle))
   :init
   (marginalia-mode))
 
 (use-package orderless
+  :ensure t
   :custom
   (completion-styles '(orderless basic))
   (completion-category-defaults nil)
   (completion-category-overrides '((file (styles partial-completion)))))
 
-(use-package vertico
-  :custom
-  (vertico-cycle t) ;; Enable cycling for `vertico-next/previous'
-  :init
-  (vertico-mode))
+(use-package rainbow-delimiters
+  :ensure t
+  :hook
+  ((prog-mode emacs-lisp-mode lisp-mode erlang-mode) . rainbow-delimiters-mode)
+  :config
+  (rainbow-delimiters-mode))
+
+(use-package ruby-mode
+  :config
+  (defun my-ruby-mode-hook ()
+    (set-fill-column 80)
+    (add-hook 'before-save-hook 'delete-trailing-whitespace nil 'local)
+    (setq ruby-insert-encoding-magic-comment nil))
+  (add-hook 'ruby-mode-hook 'my-ruby-mode-hook))
+
+(use-package smartparens
+  :ensure t
+  :bind
+  ("<f7>" . 'smartparens-mode)
+  :config
+  (require 'smartparens-config)
+  (smartparens-global-mode t))
 
 (use-package solaire-mode
+  :ensure t
   :config
   (solaire-global-mode +1))
+
+(use-package transient
+  :ensure t)
+
+(use-package vertico
+  :ensure t
+  :custom
+  (vertico-cycle t)
+  :config
+  (vertico-multiform-mode)
+  (setq vertico-multiform-categories '((embark-keybinding grid)))
+  :init
+  (vertico-mode))
 
 (use-package whaler
   :ensure t
@@ -88,17 +248,9 @@
   (setq whaler-directories-alist projects-path)
   (setq whaler-include-hidden-directories nil)
   (setq whaler-default-working-directory "~")
-  (whaler-populate-projects-directories)
-  :general
-  (leader-global
-    "ff" 'find-file-in-project-by-selected
-    "sf" 'consult-fd
-    "sr" 'consult-ripgrep
-    "so" 'consult-outline
-    "sc" 'avy-goto-char
-    "sw" 'avy-goto-word-1
-    "sl" 'avy-goto-line
-    "pp" 'whaler
-    "wr" 'whaler-populate-projects-directories
-    ))
+  (whaler-populate-projects-directories))
 
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode))
